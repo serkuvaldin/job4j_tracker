203ebfc62d3556623bd8b511c39febbaec05ac97
kuvalda <hammer1505@yandex.ru>
2022-03-19 21:57:56 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -6,6 +6,7 @@ public class ElementNotFoundException extends Exception {
         for (int i = 0; i < value.length; i++) {
             if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
         }
         if (rsl == -1) {
