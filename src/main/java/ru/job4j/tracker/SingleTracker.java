d2bc6003ce4fbe6624e367da3cac609ab942b60b
kuvalda <hammer1505@yandex.ru>
2022-03-29 19:03:34 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,40 @@
+package ru.job4j.tracker;
+
+public final class SingleTracker {
+    private static Tracker tracker = null;
+
+    private SingleTracker() {
+    }
+
+    public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item[] findByName(String name) {
+        return tracker.findByName(name);
+    }
+
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id, item);
+    }
+
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+
+}
